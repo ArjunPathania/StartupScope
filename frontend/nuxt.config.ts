13e5e34f7e424bffedfb869dd3bb10e395b3320e
ArjunPathania <arjunpathania786f@gmail.com>
2025-07-01 02:58:26 +0530

    frontend  mods

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: 'http://backend:8000'
+            apiBase: process.env.API_BASE ?? 'http://localhost:8000'
     }
+  },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
   pages:true
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,18 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const message = ref('')
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:8000/api/ping')
+  message.value = res.data.message
+})
+</script>
+
 <template>
-  <div>
-    <h1>Welcome to Nuxt</h1>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold">Nuxt + FastAPI</h1>
+    <p class="mt-4 text-green-600">Response from backend: {{ message }}</p>
   </div>
 </template>
