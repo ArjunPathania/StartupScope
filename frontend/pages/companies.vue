1ecc8ae2e5fff0c0ed5492ad4baadaec4f00c225
ArjunPathania <arjunpathania786f@gmail.com>
2025-07-01 02:41:42 +0530

    minor changes to fe added axios for fe be communication

---
+++ frontend/pages/companies.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data: companies, error, refresh } = await useFetch(
+  'http://localhost:8000/api/v1/companies/', {
+    method: 'GET',
+  }
+)
+</script>
+
+<template>
+  <div>
+    <h1>Companies</h1>
+    <div v-if="companies">
+      <ul>
+        <li v-for="c in companies" :key="c.id">
+          {{ c.name }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="error">Error: {{ error }}</div>
+  </div>
+</template>
